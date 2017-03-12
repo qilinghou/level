1d448b9ef65990398610c48f1064d9baa0cfc00c
gugu <cxiangk@163.com>
2017-03-12 22:22:44 +0800

    update file

---
+++ level.php
@@ -0,0 +1,180 @@
+<?php
+$tree = new Tree;
+$tree->tree($data);
+$tree->getArray();
+$tree->get_tree();
+class Tree
+{
+    var $arr = array();
+    var $icon = array('│','├',' └');
+    var $ret = '';
+    function tree($arr=array())
+    {
+       $this->arr = $arr;
+       $this->ret = '';
+       return is_array($arr);
+    }
+  
+    /**
+    * 得到父级数组
+    */
+    function get_parent($myid)
+    {
+        $newarr = array();
+        if(!isset($this->arr[$myid])) return false;
+        $pid = $this->arr[$myid]['parentid'];
+        $pid = $this->arr[$pid]['parentid'];
+        if(is_array($this->arr))
+        {
+            foreach($this->arr as $id => $a)
+            {
+                if($a['parentid'] == $pid) $newarr[$id] = $a;
+            }
+        }
+        return $newarr;
+    }
+  
+    /**
+    * 得到子级数组
+    */
+    function get_child($myid)
+    {
+        $a = $newarr = array();
+        if(is_array($this->arr))
+        {
+            foreach($this->arr as $id => $a)
+            {
+                if($a['parentid'] == $myid) $newarr[$id] = $a;
+            }
+        }
+        return $newarr ? $newarr : false;
+    }
+  
+    /**
+    * 得到当前位置数组
+    */
+    function get_pos($myid,&$newarr)
+    {
+        $a = array();
+        if(!isset($this->arr[$myid])) return false;
+        $newarr[] = $this->arr[$myid];
+        $pid = $this->arr[$myid]['parentid'];
+        if(isset($this->arr[$pid]))
+        {
+            $this->get_pos($pid,$newarr);
+        }
+        if(is_array($newarr))
+        {
+            krsort($newarr);
+            foreach($newarr as $v)
+            {
+                $a[$v['id']] = $v;
+            }
+        }
+        return $a;
+    }
+  
+    /**
+     * -------------------------------------
+     *  得到树型结构
+     * -------------------------------------
+     */
+    function get_tree($myid, $str, $sid = 0, $adds = '', $str_group = '')
+    {
+        $number=1;
+        $child = $this->get_child($myid);
+        if(is_array($child)) {
+            $total = count($child);
+            foreach($child as $id=>$a) {
+                $j=$k='';
+                if($number==$total) {
+                    $j .= $this->icon[2];
+                } else {
+                    $j .= $this->icon[1];
+                    $k = $adds ? $this->icon[0] : '';
+                }
+                $spacer = $adds ? $adds.$j : '';
+                $selected = $id==$sid ? 'selected' : '';
+                @extract($a);
+                $parentid == 0 && $str_group ? eval("\$nstr = \"$str_group\";") : eval("\$nstr = \"$str\";");
+                $this->ret .= $nstr;
+                $this->get_tree($id, $str, $sid, $adds.$k.'&nbsp;',$str_group);
+                $number++;
+            }
+        }
+        return $this->ret;
+    }
+  
+    /**
+    * 同上一方法类似,但允许多选
+    */
+    function get_tree_multi($myid, $str, $sid = 0, $adds = '')
+    {
+        $number=1;
+        $child = $this->get_child($myid);
+        if(is_array($child))
+        {
+            $total = count($child);
+            foreach($child as $id=>$a)
+            {
+                $j=$k='';
+                if($number==$total)
+                {
+                    $j .= $this->icon[2];
+                }
+                else
+                {
+                    $j .= $this->icon[1];
+                    $k = $adds ? $this->icon[0] : '';
+                }
+                $spacer = $adds ? $adds.$j : '';
+  
+                $selected = $this->have($sid,$id) ? 'selected' : '';
+                @extract($a);
+                eval("\$nstr = \"$str\";");
+                $this->ret .= $nstr;
+                $this->get_tree_multi($id, $str, $sid, $adds.$k.'&nbsp;');
+                $number++;
+            }
+        }
+        return $this->ret;
+    }
+  
+    function have($list,$item){
+        return(strpos(',,'.$list.',',','.$item.','));
+    }
+  
+    /**
+     +------------------------------------------------
+     * 格式化数组
+     +------------------------------------------------
+     */
+    function getArray($myid=0, $sid=0, $adds='')
+    {
+        $number=1;
+        $child = $this->get_child($myid);
+        if(is_array($child)) {
+            $total = count($child);
+            foreach($child as $id=>$a) {
+                $j=$k='';
+                if($number==$total) {
+                    $j .= $this->icon[2];
+                } else {
+                    $j .= $this->icon[1];
+                    $k = $adds ? $this->icon[0] : '';
+                }
+                $spacer = $adds ? $adds.$j : '';
+                @extract($a);
+                $a['name'] = $spacer.' '.$a['name'];
+                $this->ret[$a['id']] = $a;
+                $fd = $adds.$k.'&nbsp;';
+                $this->getArray($id, $sid, $fd);
+                $number++;
+            }
+        }
+  
+        return $this->ret;
+    }
+}
+
+?>
